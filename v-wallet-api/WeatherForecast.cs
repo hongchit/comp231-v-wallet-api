f8de3abf1b3af3dec93569c8025e885e0879b74c
Hong Chit Chan <hchan157@my.centennialcollege.ca>
2024-07-09 18:30:39 -0400

    Move VS Project to root folder

---
+++ v-wallet-api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace v_wallet_api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
