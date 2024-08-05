407951aa683b6cebc901006b10dd2ac9913669d9
JC Borlagdan <>
2024-08-05 13:43:43 -0400

    update sql schema and data scripts

--- v-wallet-data-sqldb.sql
+++ v-wallet-data-sqldb.sql
@@ -1,44 +1,54 @@
 USE [v-wallet-db]
 GO
-DELETE FROM [dbo].[tbl_FinancialTransaction]
+DELETE FROM [tbl_FinancialTransaction]
 GO
-DELETE FROM [dbo].[tbl_Category]
+DELETE FROM [tbl_Category]
 GO
-DELETE FROM [dbo].[tbl_FinancialAccount]
+DELETE FROM [tbl_FinancialAccount]
 GO
-DELETE FROM [dbo].[tbl_FinancialAccountType]
+DELETE FROM [tbl_FinancialAccountType]
 GO
-DELETE FROM [dbo].[tbl_Currency]
+DELETE FROM [tbl_Currency]
 GO
-DELETE FROM [dbo].[tbl_UserProfile]
+DELETE FROM [tbl_UserProfile]
 GO
-DELETE FROM [dbo].[tbl_UserAccount]
+DELETE FROM [tbl_UserAccount]
 GO
-INSERT [dbo].[tbl_UserAccount] ([Id], [Email], [Password], [AccountType]) VALUES (N'11324cfd-16b3-476d-9df9-37665b2d5bc8', N'jcborlagdan@outlook.com', N'Password1', N'User')
+INSERT [tbl_UserAccount] ([Id], [Email], [Password], [AccountType]) VALUES (N'11324cfd-16b3-476d-9df9-37665b2d5bc8', N'jcborlagdan@outlook.com', N'Password1', N'User')
 GO
-INSERT [dbo].[tbl_UserAccount] ([Id], [Email], [Password], [AccountType]) VALUES (N'4ecf8d30-bc7b-4477-85c3-4e492d88740c', N'v-wallet@admin.com', N'5x1&3MRi\t?&', N'Admin')
+INSERT [tbl_UserAccount] ([Id], [Email], [Password], [AccountType]) VALUES (N'4ecf8d30-bc7b-4477-85c3-4e492d88740c', N'v-wallet@admin.com', N'5x1&3MRi\t?&', N'Admin')
 GO
-INSERT [dbo].[tbl_UserProfile] ([Id], [Firstname], [Lastname], [Birthdate], [UserAccountId]) VALUES (N'833673d4-9c7f-4dfb-b062-87f4532b925c', N'System', N'Admin', NULL, N'4ecf8d30-bc7b-4477-85c3-4e492d88740c')
+INSERT [tbl_UserProfile] ([Id], [Firstname], [Lastname], [Birthdate], [UserAccountId]) VALUES (N'833673d4-9c7f-4dfb-b062-87f4532b925c', N'System', N'Admin', NULL, N'4ecf8d30-bc7b-4477-85c3-4e492d88740c')
 GO
-INSERT [dbo].[tbl_UserProfile] ([Id], [Firstname], [Lastname], [Birthdate], [UserAccountId]) VALUES (N'150cb5dd-7f3a-4bbb-838d-fb6940298448', N'Jc', N'Borlagdan', NULL, N'11324cfd-16b3-476d-9df9-37665b2d5bc8')
+INSERT [tbl_UserProfile] ([Id], [Firstname], [Lastname], [Birthdate], [UserAccountId]) VALUES (N'150cb5dd-7f3a-4bbb-838d-fb6940298448', N'Jc', N'Borlagdan', NULL, N'11324cfd-16b3-476d-9df9-37665b2d5bc8')
 GO
-INSERT [dbo].[tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'62115192-d968-49cb-ba3c-78ce4d2b142d', N'Loan', NULL)
+INSERT [tbl_Currency] ([Id], [Symbol], [Country], [IsActive]) VALUES (N'09cba794-33bd-4c78-a167-93bc4b593783', N'CAD', N'Canada', 1)
 GO
-INSERT [dbo].[tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'3ea0097b-5c67-4322-b6ae-7d4dd9e436bd', N'Savings', NULL)
+INSERT [tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'62115192-d968-49cb-ba3c-78ce4d2b142d', N'Loan', NULL)
 GO
-INSERT [dbo].[tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'03edd126-968f-4fb0-bb05-abbfa566e76d', N'General', NULL)
+INSERT [tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'3ea0097b-5c67-4322-b6ae-7d4dd9e436bd', N'Savings', NULL)
 GO
-INSERT [dbo].[tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'ea3b2b04-54b4-4ddd-9ab7-e2d0d6f346ca', N'Chequing', NULL)
+INSERT [tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'03edd126-968f-4fb0-bb05-abbfa566e76d', N'General', NULL)
 GO
-INSERT [dbo].[tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'4fb16ee4-a08b-4d4d-b113-f14c03711cae', N'Cash', NULL)
+INSERT [tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'ea3b2b04-54b4-4ddd-9ab7-e2d0d6f346ca', N'Chequing', N'Chequing')
 GO
-INSERT [dbo].[tbl_Category] ([Id], [Name], [Description]) VALUES (N'6513dcff-b3d5-453e-9427-4cf2cf57cbd9', N'Shopping', NULL)
+INSERT [tbl_FinancialAccountType] ([Id], [Name], [Description]) VALUES (N'4fb16ee4-a08b-4d4d-b113-f14c03711cae', N'Cash', NULL)
 GO
-INSERT [dbo].[tbl_Category] ([Id], [Name], [Description]) VALUES (N'02fdf1d2-6878-4199-ae4c-77288ffddb6d', N'Food', NULL)
+INSERT [tbl_FinancialAccount] ([Id], [AccountName], [AccountNumber], [InitialValue], [CurrentValue], [AccountTypeId], [CurrencyId], [UserProfileId]) VALUES (N'71b59d4e-2cd0-4e04-9ba8-589ff00d5692', N'Jesus Carlo', N'12345689', CAST(100 AS Decimal(18, 0)), CAST(0 AS Decimal(18, 0)), N'ea3b2b04-54b4-4ddd-9ab7-e2d0d6f346ca', N'09cba794-33bd-4c78-a167-93bc4b593783', N'150cb5dd-7f3a-4bbb-838d-fb6940298448')
 GO
-INSERT [dbo].[tbl_Category] ([Id], [Name], [Description]) VALUES (N'b32d115b-23a4-4276-bc3d-7c105bd05f97', N'Housing', NULL)
+INSERT [tbl_Category] ([Id], [Name], [Description]) VALUES (N'6513dcff-b3d5-453e-9427-4cf2cf57cbd9', N'Shopping', NULL)
 GO
-INSERT [dbo].[tbl_Category] ([Id], [Name], [Description]) VALUES (N'70916aa6-5f82-4b15-9dbd-99c56e55d342', N'Income', NULL)
+INSERT [tbl_Category] ([Id], [Name], [Description]) VALUES (N'02fdf1d2-6878-4199-ae4c-77288ffddb6d', N'Food', NULL)
 GO
-INSERT [dbo].[tbl_Category] ([Id], [Name], [Description]) VALUES (N'ef0bf1e3-01cc-457a-9bcc-ba8c1f6df307', N'Transportation', NULL)
+INSERT [tbl_Category] ([Id], [Name], [Description]) VALUES (N'b32d115b-23a4-4276-bc3d-7c105bd05f97', N'Housing', NULL)
+GO
+INSERT [tbl_Category] ([Id], [Name], [Description]) VALUES (N'70916aa6-5f82-4b15-9dbd-99c56e55d342', N'Income', NULL)
+GO
+INSERT [tbl_Category] ([Id], [Name], [Description]) VALUES (N'ef0bf1e3-01cc-457a-9bcc-ba8c1f6df307', N'Transportation', NULL)
+GO
+INSERT [tbl_FinancialTransaction] ([Id], [Amount], [Description], [TransactionType], [AccountId], [CategoryId], [TransactionDate]) VALUES (N'7e46b34b-9ff4-4949-a357-08dcb560674c', CAST(13 AS Decimal(18, 0)), N'', N'Expense', N'71b59d4e-2cd0-4e04-9ba8-589ff00d5692', N'02fdf1d2-6878-4199-ae4c-77288ffddb6d', CAST(N'2024-08-06T19:07:00.0000000' AS DateTime2))
+GO
+INSERT [tbl_FinancialTransaction] ([Id], [Amount], [Description], [TransactionType], [AccountId], [CategoryId], [TransactionDate]) VALUES (N'4171700a-7b11-48f1-3b6a-08dcb57426a1', CAST(77 AS Decimal(18, 0)), N'Bus', N'Expense', N'71b59d4e-2cd0-4e04-9ba8-589ff00d5692', N'ef0bf1e3-01cc-457a-9bcc-ba8c1f6df307', CAST(N'2024-08-06T21:28:00.0000000' AS DateTime2))
+GO
+INSERT [tbl_FinancialTransaction] ([Id], [Amount], [Description], [TransactionType], [AccountId], [CategoryId], [TransactionDate]) VALUES (N'329b66ec-e662-4e45-3b6c-08dcb57426a1', CAST(123 AS Decimal(18, 0)), N'', N'Expense', N'71b59d4e-2cd0-4e04-9ba8-589ff00d5692', N'b32d115b-23a4-4276-bc3d-7c105bd05f97', CAST(N'2024-08-01T21:30:00.0000000' AS DateTime2))
 GO
--- v-wallet-schema-sqldb.sql
+++ v-wallet-schema-sqldb.sql
@@ -26,122 +26,27 @@ ALTER TABLE [dbo].[tbl_Currency] DROP CONSTRAINT [DF_tbl_Currency_Id]
 GO
 ALTER TABLE [dbo].[tbl_Category] DROP CONSTRAINT [DF_tbl_Category_Id]
 GO
-/****** Object:  Table [dbo].[tbl_UserProfile]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_UserProfile]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_UserProfile]
 GO
-/****** Object:  Table [dbo].[tbl_UserAccount]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_UserAccount]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_UserAccount]
 GO
-/****** Object:  Table [dbo].[tbl_FinancialTransaction]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_FinancialTransaction]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_FinancialTransaction]
 GO
-/****** Object:  Table [dbo].[tbl_FinancialAccountType]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_FinancialAccountType]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_FinancialAccountType]
 GO
-/****** Object:  Table [dbo].[tbl_FinancialAccount]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_FinancialAccount]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_FinancialAccount]
 GO
-/****** Object:  Table [dbo].[tbl_Currency]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_Currency]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_Currency]
 GO
-/****** Object:  Table [dbo].[tbl_Category]    Script Date: 2024-07-29 8:52:11 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_Category]') AND type in (N'U'))
 DROP TABLE [dbo].[tbl_Category]
 GO
-USE [master]
-GO
-/****** Object:  Database [v-wallet-db]    Script Date: 2024-07-29 8:52:11 AM ******/
-DROP DATABASE [v-wallet-db]
-GO
-/****** Object:  Database [v-wallet-db]    Script Date: 2024-07-29 8:52:11 AM ******/
-CREATE DATABASE [v-wallet-db]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'v-wallet-db', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\v-wallet-db.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'v-wallet-db_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\v-wallet-db_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [v-wallet-db].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [v-wallet-db] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET AUTO_CLOSE OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [v-wallet-db] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [v-wallet-db] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET  DISABLE_BROKER 
-GO
-ALTER DATABASE [v-wallet-db] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [v-wallet-db] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET RECOVERY FULL 
-GO
-ALTER DATABASE [v-wallet-db] SET  MULTI_USER 
-GO
-ALTER DATABASE [v-wallet-db] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [v-wallet-db] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [v-wallet-db] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [v-wallet-db] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-ALTER DATABASE [v-wallet-db] SET DELAYED_DURABILITY = DISABLED 
-GO
-ALTER DATABASE [v-wallet-db] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-EXEC sys.sp_db_vardecimal_storage_format N'v-wallet-db', N'ON'
-GO
-ALTER DATABASE [v-wallet-db] SET QUERY_STORE = ON
-GO
-ALTER DATABASE [v-wallet-db] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
-GO
-USE [v-wallet-db]
-GO
-/****** Object:  Table [dbo].[tbl_Category]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -156,7 +61,6 @@ CREATE TABLE [dbo].[tbl_Category](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_Currency]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -172,7 +76,6 @@ CREATE TABLE [dbo].[tbl_Currency](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_FinancialAccount]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -182,6 +85,7 @@ CREATE TABLE [dbo].[tbl_FinancialAccount](
 	[AccountName] [nvarchar](50) NULL,
 	[AccountNumber] [nvarchar](50) NULL,
 	[InitialValue] [decimal](18, 0) NULL,
+	[CurrentValue] [decimal](18, 0) NULL,
 	[AccountTypeId] [uniqueidentifier] NULL,
 	[CurrencyId] [uniqueidentifier] NULL,
 	[UserProfileId] [uniqueidentifier] NULL,
@@ -191,7 +95,6 @@ CREATE TABLE [dbo].[tbl_FinancialAccount](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_FinancialAccountType]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -206,7 +109,6 @@ CREATE TABLE [dbo].[tbl_FinancialAccountType](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_FinancialTransaction]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -225,7 +127,6 @@ CREATE TABLE [dbo].[tbl_FinancialTransaction](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_UserAccount]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -241,7 +142,6 @@ CREATE TABLE [dbo].[tbl_UserAccount](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tbl_UserProfile]    Script Date: 2024-07-29 8:52:11 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -301,8 +201,4 @@ ALTER TABLE [dbo].[tbl_UserProfile]  WITH CHECK ADD  CONSTRAINT [FK_tbl_UserProf
 REFERENCES [dbo].[tbl_UserAccount] ([Id])
 GO
 ALTER TABLE [dbo].[tbl_UserProfile] CHECK CONSTRAINT [FK_tbl_UserProfile_tbl_UserAccount]
-GO
-USE [master]
-GO
-ALTER DATABASE [v-wallet-db] SET  READ_WRITE 
 GO
